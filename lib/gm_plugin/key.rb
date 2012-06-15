cfb986b0263d940495e21aff71d993b81682054a
Toño Silva Portell <jsilva@jcode.cl>
2012-06-15 09:22:39 -0300

    Update master

--- lib/gm_plugin/key.rb
+++ lib/gm_plugin/key.rb
@@ -8,30 +8,29 @@ module Ym4r
     
     #Class fo the manipulation of the API key
     class ApiKey
-      #Read the API key config for the current ENV
-      unless File.exist?("#{::Rails.root}config/gmaps_api_key.yml")
-        raise GMapsAPIKeyConfigFileNotFoundException.new("File config/gmaps_api_key.yml not found")
-      else
-        env = ENV['RAILS_ENV'] || Rails.env
-        GMAPS_API_KEY = YAML.load_file("#{::Rails.root}config/gmaps_api_key.yml")[env]
-      end
       
+      cattr_accessor :key
+     
       def self.get(options = {})
+
+        raise "Not Google Maps key defined" unless @@key
+
         if options.has_key?(:key)
           options[:key]
-        elsif GMAPS_API_KEY.is_a?(Hash)
+        elsif @@key.is_a?(Hash)
           #For this environment, multiple hosts are possible.
           #:host must have been passed as option
           if options.has_key?(:host)
-            GMAPS_API_KEY[options[:host]]
+            @@key[options[:host]]
           else
-            raise AmbiguousGMapsAPIKeyException.new(GMAPS_API_KEY.keys.join(","))
+            raise AmbiguousGMapsAPIKeyException.new(@@key.keys.join(","))
           end
         else
           #Only one possible key: take it and ignore the :host option if it is there
-          GMAPS_API_KEY
+          @@key
         end
       end
+      
     end
   end
 end
